9691c2ad0b1e501ba365a222d9fd20ec499db4a8
Cry Baby Lite <83828675+crybabylite@users.noreply.github.com>
2021-05-17 10:32:59 +0100

    add sass

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,25 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
+import variables from "../sass/jsimport.scss";
 
 Vue.use(Vuetify);
 
-export default new Vuetify({
+// compile themes styles from sass
+let themes = {};
+Object.keys(variables).forEach(x => {
+  let theme = x.split("_")[0];
+  if (!themes[theme]) {
+    themes[theme] = {};
+  }
+  let key = x.split("_")[1];
+  themes[theme][key] = variables[x];
 });
+
+const vuetify = new Vuetify({
+  theme: {
+    dark: true,
+    themes
+  }
+});
+
+export default vuetify;
