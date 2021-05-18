d3a27f9dece026c26420da6e6ff0516312036373
Cry Baby Lite <83828675+crybabylite@users.noreply.github.com>
2021-05-18 18:35:46 +0100

    update store

--- src/store/index.js
+++ src/store/index.js
@@ -6,17 +6,40 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    balance: 0,
+    balance: 10000000,
     onlineStatus: 'online', // offline....
     addresses: dummData.addresses,
     contacts: dummData.contacts,
     transactions: dummData.transactions,
     utxo: dummData.utxo,
     settings: {
-      remoteNodeAddress: 'explorer.lite-cash.com:10000'
+      remoteNodeAddress: 'explorer.lite-cash.com:10000',
+      lockscreenDuration: 30,
+      transactionPasswordPrompt: true,
+      walletPassword: 'aaaaa',
+      localNode: 'local-node', // local-node or peer
+      localNodePort: 10005,
     }
   },
   mutations: {
+    SET_REMOTE_ADDRESS (state, val) {
+      state.settings.remoteNodeAddress = val;
+    },
+    SET_WALLET_PASSWORD (state, val) {
+      state.settings.walletPassword = val;
+    },
+    SET_LOCAL_NODE (state, val) {
+      state.settings.localNode = val;
+    },
+    SET_LOCAL_NODE_PORT (state, val) {
+      state.settings.localNodePort = val;
+    },
+    SET_LOCKSCREEN_DURATION (state, val = 30) {
+      state.settings.lockscreenDuration = val;
+    },
+    SET_TRANSACTION_PASSWORD_PROMPT (state, bool = true) {
+      state.settings.transactionPasswordPrompt = bool;
+    },
   },
   actions: {
   },
