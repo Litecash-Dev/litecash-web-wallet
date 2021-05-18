6f172857e1e5cd65d66e73b99045b456ab4d7157
Cry Baby Lite <83828675+crybabylite@users.noreply.github.com>
2021-05-18 10:00:47 +0100

    add online status component

---
+++ src/components/OnlineStatus.vue
@@ -0,0 +1,46 @@
+<template>
+<div>
+  <div
+    v-if="status === 'online'"
+    class="online-status online-status--online">online</div>
+  <div
+    v-else-if="status === 'offline'"
+    class="online-status online-status--offline">offline</div>
+</div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+      }
+    },
+    computed: {
+      status () {
+        return this.$store.state.onlineStatus
+      }
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+  .online-status {
+    font-size: 12px;
+  }
+  .online-status:before {
+    margin-right: 5px;
+    border: 5px solid transparent;
+    border-radius: 50%;
+    background: 0 0;
+    content: "";
+    display: inline-block;
+    height: 10px;
+    width: 10px;
+  }
+  .online-status--online:before {
+    border-color: $branding-bright-green;
+  }
+  .online-status--offline:before {
+    border-color: $branding-gray;
+  }
+</style>
